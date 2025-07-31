8ee20425faeeea40eec1503488e3cc18edda6e55
MansoorAZafar <mansoorahmadzafarwork@gmail.com>
2025-07-31 14:36:10 -0400

    updated tf to start vm

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,9 +41,64 @@ resource "azurerm_cosmosdb_sql_database" "db" {
 }
 
 resource "azurerm_cosmosdb_sql_container" "coll" {
-  name                = "Items"
+  name                = "items"
   resource_group_name = azurerm_cosmosdb_account.acc.resource_group_name
   account_name        = azurerm_cosmosdb_account.acc.name
   database_name       = azurerm_cosmosdb_sql_database.db.name
   partition_key_path  = "/id"
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "inventory-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "isub" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "ni" {
+  name                = "inventory-ni"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.isub.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "imachine" {
+  name                = "inventory-microservice-machine"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.ni.id
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("C:/Users/imsor/.ssh/id_rsa.pem.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
