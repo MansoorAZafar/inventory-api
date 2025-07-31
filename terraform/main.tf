bd59562fc66af9cf8cb088308531268a0bbc30f6
MansoorAZafar <mansoorahmadzafarwork@gmail.com>
2025-07-31 16:58:29 -0400

    updated tf to add pub ip and cd

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
   name     = "microservice"
-  location = "westus3"
+  location = "centralus"
 }
 
 resource "azurerm_cosmosdb_account" "acc" {
@@ -62,6 +62,49 @@ resource "azurerm_subnet" "isub" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_public_ip" "public_ip" {
+  name                = "inventory-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "inventory-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  subnet_id                 = azurerm_subnet.isub.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
 resource "azurerm_network_interface" "ni" {
   name                = "inventory-ni"
   location            = azurerm_resource_group.rg.location
@@ -71,6 +114,7 @@ resource "azurerm_network_interface" "ni" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.isub.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
